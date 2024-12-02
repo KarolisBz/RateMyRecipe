c774f7fdb7ec001e6da836e74d641730c197ce03
Karolis Bandziulis <g00417529@atu.ie>
2024-12-02 22:13:50 +0000

    Home page styling image added

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,12 +1,15 @@
 // Importing React
 import React from 'react';
 import '../styles/colorTheme.css';
+import '../styles/imageScales.css';
+import CoverImg from "../assests/homeCoverImg.jpg"
 
 // Content arrow function returns us the 'Content' for the Home component
 const Home = () => {
   return (
     <div>
-        <p>hi</p>
+        <img src={CoverImg} alt="Image of food" className="window-width"/>
+        <h1 className="text-center">Recipe Categories</h1>
     </div>
   );
 }
