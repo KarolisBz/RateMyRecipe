4312854e70e88eac65bfe4d54917d5b1195bea3f
Karolis Bandziulis <g00417529@atu.ie>
2024-12-04 10:00:26 +0000

    Added Carousel for cover page

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -3,13 +3,37 @@ import React from 'react';
 import '../styles/colorTheme.css';
 import '../styles/imageScales.css';
 import CoverImg from "../assests/homeCoverImg.jpg"
+import { Carousel } from 'react-bootstrap';
 
 // Content arrow function returns us the 'Content' for the Home component
 const Home = () => {
   return (
     <div>
-        <img src={CoverImg} alt="Image of food" className="window-width"/>
-        <h1 className="text-center">Recipe Categories</h1>
+      <Carousel>
+        <Carousel.Item>
+          <img src={CoverImg} alt="Image of food" className="window-width" />
+          <Carousel.Caption>
+            <h1>Recipe Categories</h1>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={CoverImg} alt="Image of food" className="window-width" />
+          <Carousel.Caption>
+            <h1>Recipe Categories</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={CoverImg} alt="Image of food" className="window-width" />
+          <Carousel.Caption>
+            <h1>Recipe Categories</h1>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
