9235857c0e0c811d85501b5199c1fa8a3c7e3819
Karolis Bandziulis <g00417529@atu.ie>
2024-12-04 15:22:00 +0000

    NavBar links integrated with react-router-dom for consistent UI updates

--- src/client/components/NavBar.js
+++ src/client/components/NavBar.js
@@ -3,20 +3,22 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import { Navbar } from 'react-bootstrap';
 import '../styles/colorTheme.css';
+import { NavLink } from 'react-router-dom';
 
 // building a navigation Bar Via Bootstrap
 const NavBar = () => {
   return (
         <Navbar className="greyLevel3">
                 <Container className="justify-content-center">
-                    <Nav variant="tabs" defaultActiveKey="/home">
-                        <Nav.Link href="/home">All Recipes</Nav.Link>
-                        <Nav.Link href="/AllRecipes" eventKey="link-1">Favorites</Nav.Link>
-                        <Nav.Link eventKey="link-2">Appetizers</Nav.Link>
-                        <Nav.Link eventKey="link-3">Salads</Nav.Link>
-                        <Nav.Link eventKey="link-4">Soups</Nav.Link>
-                        <Nav.Link eventKey="link-5">Desserts</Nav.Link>
-                        <Nav.Link eventKey="link-6">Main Dishes</Nav.Link>
+                    <Nav variant="tabs" defaultActiveKey={"/home"}>
+                        <Nav.Link to="/home" as={NavLink} eventKey="link-1">Home</Nav.Link>
+                        <Nav.Link to="/AllRecipes" as={NavLink} eventKey="link-2">AllRecipes</Nav.Link>
+                        <Nav.Link eventKey="link-3">Favorites</Nav.Link>
+                        <Nav.Link eventKey="link-4">Appetizers</Nav.Link>
+                        <Nav.Link eventKey="link-5">Salads</Nav.Link>
+                        <Nav.Link eventKey="link-6">Soups</Nav.Link>
+                        <Nav.Link eventKey="link-7">Desserts</Nav.Link>
+                        <Nav.Link eventKey="link-8">Main Dishes</Nav.Link>
                     </Nav>
                 </Container>
         </Navbar>
