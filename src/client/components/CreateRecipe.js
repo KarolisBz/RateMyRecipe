1ba5c1c646567df25d1fcf2e30ab32ac2f0973b4
Karolis Bandziulis <G00417529@atu.ie>
2024-12-12 22:18:01 +0000

    Create page without full functionality

--- src/client/components/CreateRecipe.js
+++ src/client/components/CreateRecipe.js
@@ -3,8 +3,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
-import { Card, ListGroup } from "react-bootstrap";
-import { Form } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 
 // Read content to be imported
 const CreateRecipe = () => {
@@ -45,16 +44,52 @@ const CreateRecipe = () => {
   };
 
   return (
-    <div style={{ padding: '2vh', width: '50vw', minHeight: '80vh', margin: 'auto', marginTop: '0.5rem'}} className="greyLevel3">
+    <div style={{ padding: '2vh', width: '50vw', minHeight: '80vh', margin: 'auto', marginTop: '0.5rem' }} className="greyLevel3">
       <Form>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Label><strong>Title</strong></Form.Label>
+          <Form.Control type="text" required/>
+        </Form.Group>
+        <Form.Label><strong>Category</strong></Form.Label>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Select aria-label="Category:">
+            <option>Open this select menu</option>
+            <option value="1">Mains</option>
+            <option value="2">Desserts</option>
+            <option value="3">Soups</option>
+            <option value="3">Salads</option>
+            <option value="3">Appetizers</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label><strong>Date Posted:</strong></Form.Label>
+          <Form.Control type="text" required/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label><strong>Calories:</strong></Form.Label>
+          <Form.Control type="text" required/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label><strong>Fat:</strong></Form.Label>
+          <Form.Control type="text" required/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label><strong>Protein:</strong></Form.Label>
+          <Form.Control type="text" required/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label><strong>Salt:</strong></Form.Label>
+          <Form.Control type="text" required/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label><strong>Thumbnail:</strong></Form.Label>
+          <Form.Control type="text" required/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Example textarea</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+          <Form.Label><strong>Description</strong></Form.Label>
+          <Form.Control as="textarea" rows={3} required/>
         </Form.Group>
+        <Button type="submit" variant="outline-success" style={{ width: '100%' }}>Submit form</Button>
       </Form>
     </div >
   );
